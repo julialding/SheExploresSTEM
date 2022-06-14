71c12f35e0c2b908a6adccb5f16a372b1abb8125
juliading <jding713@gmail.com>
2022-06-14 13:26:14 -0500

    Initial Commit

---
+++ SheExploresSTEM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SheExploresSTEM
+//
+//  Created by Julia D on 6/14/22.
+//  Copyright © 2022 Julia D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SheExploresSTEMTests/SheExploresSTEMTests.swift
@@ -0,0 +1,34 @@
+//
+//  SheExploresSTEMTests.swift
+//  SheExploresSTEMTests
+//
+//  Created by Julia D on 6/14/22.
+//  Copyright © 2022 Julia D. All rights reserved.
+//
+
+import XCTest
+@testable import SheExploresSTEM
+
+class SheExploresSTEMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
